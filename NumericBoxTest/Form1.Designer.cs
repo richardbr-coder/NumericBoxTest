4d883a06c7e65bc6b46c97c7064e7e8449926510
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:39:54 -0230

    Add files via upload

---
+++ NumericBoxTest/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace NumericBoxTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericBox1 = new NumericBox.NumericBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numericBox1
+            // 
+            this.numericBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.numericBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.numericBox1.Location = new System.Drawing.Point(303, 12);
+            this.numericBox1.Name = "numericBox1";
+            this.numericBox1.NegativeNumber = true;
+            this.numericBox1.NegativeNumberColor = System.Drawing.Color.Red;
+            this.numericBox1.Size = new System.Drawing.Size(150, 20);
+            this.numericBox1.TabIndex = 0;
+            this.numericBox1.Load += new System.EventHandler(this.numericBox1_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(119, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(511, 134);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(146, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What do you notice about he control\'s functionality?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NumericBox.NumericBox numericBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
